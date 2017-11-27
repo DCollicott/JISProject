0345f0e1538f5758d0f615da7f6c61e73e01b582
DCollicott <duane@collicott.com>
2017-11-26 19:58:53 -0500

    Added engine tests

---
+++ JISProject.Test/EngineTests/OperationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+ 
+
+namespace JISProject.Test.EngineTests
+{
+    [TestFixture]
+    [Category("EngineTests.OperationTests")]
+    public class OperationTests
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void SubtractionTest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void MultiplicationTest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void DivisionTest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void InvalidValueTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
