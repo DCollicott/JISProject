2f11a4a7b37a0ea3ba328050f3da8b0240bee54c
DCollicott <duane@collicott.com>
2017-11-26 23:20:59 -0500

    Created app with basic calculator buttons.

---
+++ JISProject.App/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using JISProject.App.Models;
+
+
+namespace JISProject.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(CalcModel Model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ JISProject.App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JISProject.App.MvcApplication" Language="C#" %>
