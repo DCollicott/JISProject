2585dbb8c2251a163bd693aa84b88bd37199ad66
DCollicott <duane@collicott.com>
2017-11-26 21:18:21 -0500

    Operation tests complete. Could use more edge cases.

--- JISProject.Test/EngineTests/OperationTests.cs
+++ JISProject.Test/EngineTests/OperationTests.cs
@@ -25,28 +25,52 @@ namespace JISProject.Test.EngineTests
         [Test]
         public void SubtractionTwoNumbers()
         {
-            throw new NotImplementedException();
+            var A = new PrimitiveEntry(23.45m);
+            var B = new PrimitiveEntry(34.56m);
+            var Z = new OperationEntry(A, OperationTypeOptions.Subtraction, B);
+
+            var Rslt = Z.Process();
+
+            Assert.That(Rslt, Is.EqualTo(-11.11m));
         }
 
 
         [Test]
         public void MultiplyTwoNumbers()
         {
-            throw new NotImplementedException();
+            var A = new PrimitiveEntry(23.45m);
+            var B = new PrimitiveEntry(34.56m);
+            var Z = new OperationEntry(A, OperationTypeOptions.Multiplication, B);
+
+            var Rslt = Z.Process();
+
+            Assert.That(Rslt, Is.EqualTo(810.432m));
         }
 
 
         [Test]
         public void DivideTwoNumbers()
         {
-            throw new NotImplementedException();
+            var A = new PrimitiveEntry(23.45m);
+            var B = new PrimitiveEntry(34.56m);
+            var Z = new OperationEntry(A, OperationTypeOptions.Division, B);
+
+            var Rslt = Z.Process();
+
+            Assert.That(Rslt, Is.EqualTo(A.Value / B.Value));
         }
 
 
         [Test]
-        public void InvalidValueTest()
+        public void DivideByZero()
         {
-            throw new NotImplementedException();
+            var A = new PrimitiveEntry(23.45m);
+            var B = new PrimitiveEntry(0m);
+            var Z = new OperationEntry(A, OperationTypeOptions.Division, B);
+
+            var Rslt = Z.Process();
+
+            Assert.That(Rslt, Is.EqualTo(null));
         }
     }
 }
